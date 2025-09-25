b5ca5cda9ad411959a7e4053e220d2540fb88d01
Raissa <raissamalves@gmail.com>
2025-09-25 16:04:14 -0300

    feat: add SecondaryButton and BaseUi components to app and fix base-ui class name

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from "./components/navbar/navbar";
 import { ItemCertificado } from "./components/item-certificado/item-certificado";
+import { SecondaryButton } from './shared/secondary-button/secondary-button';
+import { BaseUi } from './shared/base-ui/base-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, ItemCertificado],
+  imports: [Navbar, ItemCertificado, SecondaryButton, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
