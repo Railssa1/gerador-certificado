1ba16ad82115daba4f6031d95ef56d5762bcadd9
Raissa <raissamalves@gmail.com>
2025-09-25 21:49:31 -0300

    feat: criar componente PageHeader e remover títulos do componente Certificados

--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../shared/secondary-button/secondary-button";
 import { ItemCertificado } from "../../components/item-certificado/item-certificado";
+import { PageHeader } from "../../shared/page-header/page-header";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButton, ItemCertificado],
+  imports: [SecondaryButton, ItemCertificado, PageHeader],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css'
 })
